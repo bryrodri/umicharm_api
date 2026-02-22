6fbc6ca3cea7e3a723d7d141c9f951f1de7d5df9
Bryan Rodriguez <bryrodri1@gmail.com>
2026-02-22 19:50:19 -0400

    change port 8080

--- src/main.ts
+++ src/main.ts
@@ -39,6 +39,7 @@ async function bootstrap() {
     },
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  //await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080, '0.0.0.0');
 }
 bootstrap();
